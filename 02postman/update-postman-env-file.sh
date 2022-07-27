0ef87bcd618bfdb969de375593234c2a215b6481
Sucheta.G <sucheta.ghosh@fourtech.com>
2022-07-27 14:46:57 +0530

    Update postman env file

--- 02postman/update-postman-env-file.sh
+++ 02postman/update-postman-env-file.sh
@@ -6,7 +6,7 @@
 echo "Running update-postman-env-file.sh"
 
 api_gateway_url=`aws cloudformation describe-stacks \
-  --stack-name petstore-api-stack5 \
+  --stack-name petstore-api-stack101 \
   --query "Stacks[0].Outputs[*].{OutputValueValue:OutputValue}" --output text`
 
 echo "API Gateway URL:" ${api_gateway_url}
